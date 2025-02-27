64fbbec21e6f2b14d9008d3a28a3c329fd634b72
Lawrence-Tang <tangzongsheng@gmail.com>
2025-02-27 18:53:28 +0800

    bump to openwrt-24.10

--- rk3588/r8125/install.sh
+++ rk3588/r8125/install.sh
@@ -3,6 +3,6 @@ set -eu
 ROOTFS_DIR=$1
 
 (cd ${ROOTFS_DIR} && {
-	[ -e etc/modules.d/10-r8169 ] && rm etc/modules.d/10-r8169
+	rm -f etc/modules.d/*-r8169
 	echo 'r8125' > etc/modules.d/10-r8125
 })
